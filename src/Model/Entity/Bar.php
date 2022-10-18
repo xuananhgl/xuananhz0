75fae15ac91e030d317dba2469b7f00d7eefd760
xuananhgl <109198646+xuananhgl@users.noreply.github.com>
2022-10-18 07:02:09 +0700

    Add files via upload

---
+++ src/Model/Entity/Bar.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Model\Entity;
+use Cake\Collection\Collection;
+use Cake\ORM\Behavior\Translate\TranslateTrait;
+use Cake\ORM\Entity;
+class Bar extends Entity
+{
+  use TranslateTrait;
+
+}
+?>
